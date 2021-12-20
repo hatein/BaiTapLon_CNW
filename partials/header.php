43fa830959ed60b14d7eda27d968d54915a498f7
69_1951061112_HoangThiThuUyen <87602377+HoangUyn8886@users.noreply.github.com>
2021-12-20 10:12:03 +0700

    Update header.php

--- partials/header.php
+++ partials/header.php
@@ -1,37 +1,37 @@
 <header class="header">
     <div class="box1">
-        <a class="box1_logo" href="./home.php">
-            <img src="./assets/img/logo.png" alt="logo">
+        <a class="box1_logo" href="home.php">
+            <img src="assets/img/logo.png" alt="logo">
         </a>
-        <a href="./search.php" class="box1_search_mb">
-            <img src="./assets/img/icon_tim_kiem.svg" alt="" srcset="">
+        <a href="search.php" class="box1_search_mb">
+            <img src="assets/img/icon_tim_kiem.svg" alt="" srcset="">
         </a>
         <div class="box1_search">
-            <label for="search_pc"><img src="./assets/img/icon_tim_kiem.svg" alt="icon_tim_kiem" srcset=""></label>
+            <label for="search_pc"><img src="assets/img/icon_tim_kiem.svg" alt="icon_tim_kiem" srcset=""></label>
             <input id="search" type="text" placeholder="Tìm kiếm trên Facebook">
         </div>
     </div>
     <div class="box2">
         <a class="box2_logo" href="./home.php">
-            <img src="./assets/img/fb_logo.svg" alt="logo" srcset="">
+            <img src="assets/img/fb_logo.svg" alt="logo" srcset="">
         </a>
     </div>
     <div class="box3">
-        <a class="box3_user" href='./user.php'>
-            <img src="" alt="" srcset="./assets/img/avt_null.jpg">
+        <a class="box3_user" href='user.php'>
+            <img src="" alt="" srcset="assets/img/avt_null.jpg">
             <span>Hoàng Thị Thu Uyên</span>
         </a>
-        <a class="box3_signout" href="./process/process-signout.php">Đăng xuất</a>
+        <a class="box3_signout" href="process/process-signout.php">Đăng xuất</a>
         <div class="menu_mb">
             <div class="menu_mb_item"></div>
             <div class="menu_mb_item"></div>
             <div class="menu_mb_item"></div>
 
             <div class="menu_mb_tab">
-                <a href="./search.php">Tìm kiếm</a>
-                <a href="./hobby.php">Sở thích</a>
-                <a href="./change-password.php">Đổi mật khẩu</a>
-                <a href="./process/process-signout.php">Đăng xuất</a>
+                <a href="search.php">Tìm kiếm</a>
+                <a href="hobby.php">Sở thích</a>
+                <a href="change-password.php">Đổi mật khẩu</a>
+                <a href="process/process-signout.php">Đăng xuất</a>
             </div>
         </div>
     </div>
